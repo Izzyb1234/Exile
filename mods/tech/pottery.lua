17ea501014ef993100a05b3e7ca870d9ff203aaa
Jan Wielkiewicz <tona_kosmicznego_smiecia@interia.pl>
2022-10-23 02:14:31 +0200

    Lantern: animate the fire, rename to english, add assembly stages.

--- mods/tech/pottery.lua
+++ mods/tech/pottery.lua
@@ -638,14 +638,89 @@ minetest.override_item("tech:clay_water_pot_freshwater",{
 	end
 })
 
--- Lataren
-minetest.register_node("tech:lataren", {
-	description = S("Lataren"),
+-- Lantern case
+minetest.register_node("tech:lantern_case", {
+	description = S("Lantern Case"),
 	tiles = {
-            {name = "tech_lataren.png"},
+            {name = "tech_lantern_case.png"},
 	},
 	drawtype = "mesh",
-        mesh = "lataren.obj",
+        mesh = "lantern.obj",
+	stack_max = minimal.stack_max_medium,
+	sunlight_propagates = true,
+        use_texture_alpha = true,
+	paramtype = "light",
+	paramtype2 = "facedir",
+	node_box = {
+            type = "fixed",
+            fixed = {-0.5, -0.5, -0.5, 0.5, -0.45, 0.5},
+        },
+	groups = {dig_immediate=3, temp_pass = 1, falling_node = 1},
+	sounds = nodes_nature.node_sound_stone_defaults(),
+})
+
+-- Lantern case + wick
+minetest.register_node("tech:lantern_case_wick", {
+	description = S("Lantern Case with a wick"),
+	tiles = {
+            {name = "tech_lantern_case.png"},
+	},
+        overlay_tiles = {
+            {name = "tech_lantern_wick.png"},
+        },
+	drawtype = "mesh",
+        mesh = "lantern.obj",
+	stack_max = minimal.stack_max_medium,
+	sunlight_propagates = true,
+        use_texture_alpha = true,
+	paramtype = "light",
+	paramtype2 = "facedir",
+	node_box = {
+            type = "fixed",
+            fixed = {-0.5, -0.5, -0.5, 0.5, -0.45, 0.5},
+        },
+	groups = {dig_immediate=3, temp_pass = 1, falling_node = 1},
+	sounds = nodes_nature.node_sound_stone_defaults(),
+})
+
+-- Lantern case + glass + wick
+minetest.register_node("tech:lantern_unlit", {
+	description = S("Unlit Lantern"),
+	tiles = {
+            {name = "tech_lantern_case_glass.png"},
+	},
+        overlay_tiles = {
+            {name = "tech_lantern_wick.png"},
+        },
+	drawtype = "mesh",
+        mesh = "lantern.obj",
+	stack_max = minimal.stack_max_medium,
+	sunlight_propagates = true,
+        use_texture_alpha = true,
+	paramtype = "light",
+	paramtype2 = "facedir",
+	node_box = {
+            type = "fixed",
+            fixed = {-0.5, -0.5, -0.5, 0.5, -0.45, 0.5},
+        },
+	groups = {dig_immediate=3, temp_pass = 1, falling_node = 1},
+	sounds = nodes_nature.node_sound_stone_defaults(),
+})
+
+-- Lantern
+minetest.register_node("tech:lantern_lit", {
+	description = S("Lit Lantern"),
+	tiles = {
+            {name = "tech_lantern_case_glass.png"},
+	},
+        overlay_tiles = {
+            {
+                name = "tech_lantern_animation.png",
+                animation = {type = "vertical_frames", aspect_w = 48, aspect_h = 48, length = 2}
+            },
+        },
+	drawtype = "mesh",
+        mesh = "lantern.obj",
 	stack_max = minimal.stack_max_medium,
 	sunlight_propagates = true,
 	light_source = 11,
